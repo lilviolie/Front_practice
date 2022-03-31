a45ca11477c380000d449d34eba42856693cc118
Ryokuman <ryokuman@naver.com>
2022-03-31 20:28:24 +0900

    Finished my React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [search, setSearch] = useState("");
+
+  console.log("I run all the time");
+
+  useEffect(() => {
+    console.log(`${search} has been searched`);
+  }, [search]);
+
+  useEffect(() => {
+    console.log("I run counter is change");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run search & counter is change");
+  }, [search, counter]);
+
+  function onClick() {
+    return setCounter((a) => a + 1);
+  }
+
+  const onSearch = (event) => setSearch(event.target.value);
+
   return (
     <div>
-      <h1>Welcom Back!!!</h1>
-      <Button text="Continue" />
+      <input onChange={onSearch} type="text" placeholder="Search Here!"></input>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text="Click me!" />
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,17 @@
-function Button({ text }) {
-  return <button>{text}</button>;
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({ text, onClick }) {
+  return (
+    <button onClick={onClick} className={styles.btn}>
+      {text}
+    </button>
+  );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired,
+};
 
 export default Button;
