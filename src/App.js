4a10aea4139085ef67fdb9d1790b117efaf8f54a
Ryokuman <ryokuman@naver.com>
2022-03-31 21:26:09 +0900

    First_commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
+import Login from "./Login/Login.js";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [search, setSearch] = useState("");
-
-  console.log("I run all the time");
-
-  useEffect(() => {
-    console.log(`${search} has been searched`);
-  }, [search]);
-
-  useEffect(() => {
-    console.log("I run counter is change");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run search & counter is change");
-  }, [search, counter]);
-
-  function onClick() {
-    return setCounter((a) => a + 1);
-  }
-
-  const onSearch = (event) => setSearch(event.target.value);
-
-  return (
-    <div>
-      <input onChange={onSearch} type="text" placeholder="Search Here!"></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={onClick} text="Click me!" />
-    </div>
-  );
-}
+const App = () => {
+  <div>
+    <Login />
+  </div>;
+};
 
 export default App;
---
+++ src/Login/Login.js
@@ -0,0 +1,12 @@
+import styles from "./Login.moudle.css";
+
+const Login = () => {
+  <div>
+    <div>
+      <h1 className={styles.headLine}>Day Log</h1>
+    </div>
+    <div></div>
+  </div>;
+};
+
+export default Login;
